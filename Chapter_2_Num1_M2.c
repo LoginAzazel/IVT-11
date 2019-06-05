a190e15877762737e9140c745339c1528a6bd609
ArcaneFox <43112945+LoginAzazel@users.noreply.github.com>
2019-06-06 00:50:12 +0300

    Update Chapter_2_Num1_M2.c

--- Chapter_2_Num1_M2.c
+++ Chapter_2_Num1_M2.c
@@ -5,23 +5,44 @@ double Formul(double, double, double);
 
 int main ()
 {
-    double ConstD, Accuracy, ConstC;
+    double D, Accuracy, C;
     double X=0, X1=0, Fx, dFx; 
     int range = 0;
     printf("\n\n Программа ищет пересечение графика с осью абцис функции:\n"
            "\n Sin(X*C) - D = 0 \n\n");
     printf ("Введите коофициент наклона (С): "); 
-    scanf ("%lf", &ConstC);
+    scanf ("%lf", &C);
     printf ("Введите смещение по Y (D): "); 
-    scanf ("%lf", &ConstD);
+    scanf ("%lf", &D);
     printf ("Введите точность где Х < 1 (В формате 0.01): ");
     scanf ("%lf", &Accuracy);
+
+    if( D > 1 || D < - 1) 
+    { 
+        printf ("Нет точек пересечения\n"); 
+        return 0;
+    }
+    
+    while( Formul(C,D,X)*Formul(C,D,X1) >= 0 && range < 30)
+    {
+        X=X1;
+        X1 += 0.5;
+        range++;
+    }
+    range = 0;
+    while( Formul(C,D,X)*Formul(C,D,X1) >= 0 && range < 30)
+    {
+        X=X1;
+        X1 -= 0.5;
+        range++;
+    }
+    range = 0;
 
     do
     {
         range++;
-        Fx = Formul(ConstC, ConstD, X);
-        dFx = ConstC * cos(X*ConstC);
+        Fx = Formul(C, D, X);
+        dFx = C * cos(X*C);
         X -= Fx/dFx;
     } while( Accuracy < dFx && range < 10000);
     printf("%lf\n", X);
