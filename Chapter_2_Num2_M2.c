0e5d1cc06c730b8442d365b0953386cbcb326547
ArcaneFox <43112945+LoginAzazel@users.noreply.github.com>
2019-06-06 01:03:01 +0300

    Update Chapter_2_Num2_M2.c

--- Chapter_2_Num2_M2.c
+++ Chapter_2_Num2_M2.c
@@ -5,25 +5,41 @@ double Formul(double, double, double);
 
 int main ()
 {
-    double ConstD, Accuracy, ConstC;
+    double D, Accuracy,C;
     double X=0, X1=0, Fx, dFx; 
     int range = 0;
     printf("\n\n Программа ищет пересечение графика с осью абцис функции:\n"
            "\n e^Cx - D = 0 \n\n");
     printf ("Введите С: "); 
-    scanf ("%lf", &ConstC);
+    scanf ("%lf", &C);
     printf ("Введите смещение по Y (D): "); 
-    scanf ("%lf", &ConstD);
+    scanf ("%lf", &D);
     printf ("Введите точность где Х < 1 (В формате 0.01): ");
     scanf ("%lf", &Accuracy);
 
+     while( Formul(C,D,X)*Formul(C,D,X1) >= 0 && range < 30)
+    {
+        X=X1;
+        X1 += 0.1;
+        range++;
+    }
+    range = 0;
+    while( Formul(C,D,X)*Formul(C,D,X1) >= 0 && range < 30)
+    {
+        X=X1;
+        X1 -= 0.1;
+        range++;
+    }
+    range = 0;
+
     do
     {
+        X = X1;
         range++;
-        Fx = Formul(ConstC, ConstD, X);
-        dFx = ConstC * pow(2.71, ConstC*X);
-        X -= Fx/dFx;
-    } while( Accuracy < dFx && range < 10000);
+        Fx = Formul(C, D, X1);
+        dFx = C * pow(2.71, C*X1);
+        X1 -= Fx/dFx;
+    } while( Accuracy < fabs(X - X1) && range < 10000);
     printf("%lf\n", X);
     return 0;
 }
