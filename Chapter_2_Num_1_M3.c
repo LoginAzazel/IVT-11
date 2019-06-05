2ff8cfcf0dd7bc1f6d9679fb50f3d1833477856f
ArcaneFox <43112945+LoginAzazel@users.noreply.github.com>
2019-06-05 22:02:14 +0300

    Create Chapter_2_Num_1_M3.c

---
+++ Chapter_2_Num_1_M3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h> 
+
+double Formul(double, double, double);
+
+int main ()
+{
+    double D, Accuracy, C;
+    double End, First = 0; 
+    double X1=0.01, X=0;
+    int range = 0;
+    printf("\n\n Программа ищет пересечение графика с осью абцис функции:\n"
+            "\n Sin(X*C) - D = 0 \n\n");
+    printf ("Введите С: "); 
+    scanf ("%lf", &C);
+    printf ("Введите смещение по Y (D): "); 
+    scanf ("%lf", &D);
+    printf ("Введите точность где Х < 1 (В формате 0.01): ");
+    scanf ("%lf", &Accuracy);
+
+    while( Formul(C,D,X)*Formul(C,D,X1) >= 0)
+    {
+        X=X1;
+        X1 += 0.5;
+    }
+    printf("\n-------------\n");
+    do
+    {
+        range++;
+        First = X1 - Formul(C,D,X1)*( (X1 - X)/(Formul(C,D,X1) - Formul(C,D,X) ) );
+        X = X1;
+        X1 = First;
+            printf("%.8lf | %d\n", X1, range);
+    } while( Accuracy < fabs(X1 - X) && range < 1000);
+    return 0;
+}
+
+double Formul( double C, double D, double X)
+{
+     return sin(X*C) - D;
+}
