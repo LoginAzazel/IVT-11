b44924bb46b186757984bed81cc4b7c3ea7fb10f
Nikita Kukuschin <43112945+LoginAzazel@users.noreply.github.com>
2020-11-10 20:57:30 +0300

    Update Lab-3-3.cs

--- C#/Exam_Debt/Lab-3-3.cs
+++ C#/Exam_Debt/Lab-3-3.cs
@@ -4,23 +4,29 @@ namespace LearningProg
 {
     class Program
     {   // Лабораторная работа 3. Задание 3. Вариант 8
-            static void Main(string[] args)
-            {
-                double result;
-                int E = 22;
-                double x = 1.5;
+        static void Main(string[] args)
+        {
+            double result, Cache = 1, preCache;
+            double e = 0.0001;
+            double x = 1.5;
+            int i = 0;
 
-                Console.WriteLine("N" + "\t| arctg x\t|\tresult");
-                Console.WriteLine("---------------------------------------");
-                for (int j = 1; x <= 10; x += 0.5, j++)
+            Console.WriteLine("N\t| arctg x\t|\tresult rad\t| result deg");
+            Console.WriteLine("-------------------------------------------------------------");
+
+            for (int j = 1; x <= 10; x += 0.5, j++)
+            {
+                result = Math.PI / 2;
+                do
                 {
-                    result = 3.14 / 2;
-                    for (int i = 0; i < E; i++)
-                    {
-                        result += Math.Pow(-1, i + 1) / ((2 * i + 1) * Math.Pow(x, 2 * i + 1));
-                    }
-                    Console.WriteLine(j + "\t| arctg " + x + "\t=\t" + Math.Round(result, 5));
-                }
+                    preCache = Cache;
+                    Cache = Math.Pow(-1, i + 1) / ((2 * i + 1) * Math.Pow(x, 2 * i + 1));
+                    result += Cache;
+                    i++;
+                } while (Math.Abs(preCache - Cache) > e);
+                Console.WriteLine(j + "\t| arctg " + x + "\t|\t" + Math.Round(result, 8) + "\t| " + Math.Round(result*180/Math.PI, 8));
+                Cache = 1; i = 0;
             }
+        }
     }
 }
